844fcbb05d11d38fa061497baec7adfa8e794332
Haidarr-h <haidarapps12@gmail.com>
2026-05-27 20:47:49 +0700

    y

--- src/services/ai.js
+++ src/services/ai.js
@@ -1,6 +1,7 @@
 import api from "./api";
 const token = localStorage.getItem("evalyn_token");
 
+// ai
 export const analyzeQuiz = (quiz_id) => {
   return api.post(
     `/ai/analyze-quiz/${quiz_id}?model_name=azure`,
